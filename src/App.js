0f5f8e00372f75ded630a5d68d0134a63b7f1ce6
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-05-19 10:10:54 -0700

    test with Zack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-//import axios from 'axios';
+import axios from 'axios';
 import "./App.css";
 import Characters from "./components/Character";
 import { data } from "./mocks/handlers";
 
 const App = () => {
-  const [data, setdata] = useState("");
-  const [res, setResults] = useState([]);
+  const [data, setData] = useState("");
+  const [results, setResults] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -14,27 +14,30 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const search = () => {
-    fetch(`https://swapi.dev/api/people/?search=${data}`)
-      .then((res) => res.json())
-      .then((data) => setResults(data.setResults))
+    axios.get(`https://swapi.dev/api/people/`)
+      .then((data) => {setResults(data.data)
+     // console.log('axios', data.data)
+      })
       .catch((err) => console.error(err));
 
-    setResults(res);
-    console.log(res);
+    //setResults(res);
+    //console.log(results);
   };
+  console.log(results);
 
   useEffect(() => {
+    console.log('start');
     search();
-  }, [data]);
+    console.log('end');
+  }, []);
 
   return (
     <div className="app">
       <h1>React Wars</h1>
-      <Characters 
-        name={data.name}
-        birthDay={data.birthYear}
-        homeWorld={data.homeWorld}
-      />
+     <Characters char={results}/>
+      
+  
+        
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,14 +3,17 @@ import React from "react";
 
 
 
-const Character = ({name, birthYear, homeWorld}) => {
-    return (
-        <div>
-            <h3>Name: {name}</h3>
-            <p>Birth Day: {birthYear}</p>
-            <p>Home: {homeWorld}</p>
-        </div>
-    );
-};
+const Character = ({char}) => (
+    <div>
+    {
+        char.forEach(element => {
+            <h3>Name: {element?.name}</h3>
+        })
+        }
+    {/* <p>Birth Day: {birthYear}</p>
+    <p>Home: {homeWorld}</p> */}
+</div>
+)
+    
 
 export default Character; 
