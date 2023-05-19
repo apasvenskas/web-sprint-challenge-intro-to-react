254f7108cd93c5eebb4fa84671424198dedd7c12
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-05-18 19:28:13 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+//import axios from 'axios';
+import "./App.css";
+import Characters from "./components/Character";
+import { data } from "./mocks/handlers";
 
 const App = () => {
+  const [data, setdata] = useState("");
+  const [res, setResults] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const search = () => {
+    fetch(`https://swapi.dev/api/people/?search=${data}`)
+      .then((res) => res.json())
+      .then((data) => setResults(data.setResults))
+      .catch((err) => console.error(err));
+
+    setResults(res);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    search();
+  }, [data]);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="app">
+      <h1>React Wars</h1>
+      <Characters 
+        name={data.name}
+        birthDay={data.birthYear}
+        homeWorld={data.homeWorld}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+//https://swapi.dev/api/people/?search
+/*
+<SearchForm query={query} setQuery={setQuery} />
+  {results.map((result) => (
+        <Characters
+          keys={result.name}
+          name={result.name}
+          birthYear={result.birthYear}
+          homeWorld={result.homeWorld}
+        />
+      ))};
+
+*/
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from "react";
+
+
+
+const Character = ({name, birthYear, homeWorld}) => {
+    return (
+        <div>
+            <h3>Name: {name}</h3>
+            <p>Birth Day: {birthYear}</p>
+            <p>Home: {homeWorld}</p>
+        </div>
+    );
+};
+
+export default Character; 
